873ef6fd701e8955f95d8e925b139066a8784955
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-08-03 14:59:41 +0900

    Update 37.py

--- 37.py
+++ 37.py
@@ -12,8 +12,8 @@ for a in range(1, n + 1):
 for _ in range(m):
     a, b, c = map(int, input().split())
     # 가장 짧은 간선 정보만 저장
-    graph[a][b] = min(graph[a][b], c)
-
+    graph[a][b] = min(graph[a][b], c)   # if c < graph[a][b]: graph[a][b] = c
+    
 # 점화식 수행
 for k in range(1, n + 1):
     for a in range(1, n + 1):
@@ -27,3 +27,9 @@ for a in range(1, n + 1):
         else:
             print(graph[a][b], end=' ')
     print()
+    
+"""
+전형적인 최단 경로문제
+도시의 개수(노드)n이 100 이하 정수이므로 플로이드 워셜 알고리즘 이용
+간선정보만 살짝 바꾸면 된다.
+"""
