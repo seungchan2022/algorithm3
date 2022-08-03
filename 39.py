3306e3d079680cbdaba78b1d4a790c0f57963763
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-08-03 16:34:35 +0900

    Update 39.py

--- 39.py
+++ 39.py
@@ -20,8 +20,9 @@ for tc in range(int(input())):
 
     x, y = 0, 0 # 시작 위치
     # 시작노드로 가기위한 비용은 (0, 0)위치의 값으로 설정, 큐에 삽입
-    q = [(graph[x][y], x, y)]
+    q = []
     distance[x][y] = graph[x][y]
+    heapq.heappush(q, (graph[x][y], x, y))
 
     # 다익스트라 수행
     while q:
@@ -35,9 +36,18 @@ for tc in range(int(input())):
             # 범위
             if nx < 0 or nx >= n or ny < 0 or ny >= n:
                 continue
-            cost = dist + graph[nx][ny]
+            cost = dist + graph[nx][ny]     # graph[nx][ny] = 좌표값(거리값 = 비용)
             # 현재 노드를 거쳐, 다른 노드로 이동하는 거리가 더 짧은 경우
             if cost < distance[nx][ny]:
-                distance[nx][ny] = cost
-                heapq.heappush(q, (cost, nx, ny))
+                distance[nx][ny] = cost     # cost = distance[nx][ny]로 하면 안됌(이해 X)
+                heapq.heappush(q, (cost, nx, ny))   # (거리, 노드) -> (x, y)좌표 값 하나를 노드로 봄
     print(distance[n - 1][n - 1])
+
+
+"""
+문제에서 입력 자체가 2차원 배열로 들어기 때문에 N X N 인접 행렬을 이용해 그래프로 표현, 그래서 최단 거리 테이블도 2차원으로 표현
+N의 범의가 최대 125로 작게 느낄 수 있지만, 2차원 공간이기 때문에 전체 노드의 개수는 N^2으로 10000을 넘을수 있다
+따라서 플로이드 워셜 알고리즘이 아니라 다익스트라 알고리즘을 이용
+(x, y)좌표 값 하나를 노드로 보는데, (x, y)에서 또 다른 (x, y)로 가는 비용은 도착하는 (x, y)좌표 값으로 한다.
+ex) (1, 1) -> (1, 2)로 가는 비용은 (1, 2)좌표에 있는 값!
+"""
