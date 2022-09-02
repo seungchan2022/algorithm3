c95691c992bdb68c4ab49b609a3721c0e0cf45f1
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-09-02 15:00:57 +0900

    Update 15.py

--- 15.py
+++ 15.py
@@ -29,3 +29,42 @@ for i in range(1, n + 1):
 # 만약 최단거리가 k인 도시가 없으면
 if check == False:
     print(-1)
+    
+    
+"""
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [-1] * (n + 1)
+
+result = []     # 거리가 같은 노드들 담을 리스트
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+def bfs(v):
+    q = deque()
+    visited[v] += 1
+    q.append(v)
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if visited[i] == -1:
+                visited[i] = visited[now] + 1
+                q.append(i)
+
+bfs(x)
+
+for i in range(1, n + 1):
+    if visited[i] == k:
+        result.append(i)
+
+for i in result:
+    if not result:
+        print(-1)
+    else:
+        print(i)
+"""
