334ae88b8f6668310de23bef5585e246a43734b0
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-10 17:35:51 +0900

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,36 +1,36 @@
 from itertools import combinations
 
 n, m = map(int, input().split())
-chicken, house = [], []
+house = []  # 집 좌표
+chicken = []    # 치킨집 좌표
 
+graph = []
 for i in range(n):
-    data = list(map(int, input().split()))
+    graph.append(list(map(int, input().split())))
     for j in range(n):
-        if data[j] == 1:
-            house.append((i, j))
-        elif data[j] == 2:
-            chicken.append((i, j))
+        if graph[i][j] == 1:
+            house.append((i + 1, j + 1))
+        if graph[i][j] == 2:
+            chicken.append((i + 1, j + 1))
 
 # 모든 치킨집 중에서 m개의 치킨집을 뽑는 조합 계산
 candidates = list(combinations(chicken, m))
 
-# 치킨 거리의 합을 계산하는 함수
-def get_sum(candidate):
-    result = 0
-    # 모든 집에 대하여
+# 치킨 거리의 합 계산 하는 함수
+def sum(candidate):
+    value = 0   # 도시의 치킨 거리(치킨 거리의 합)
+    # 모든 집에 대해서
     for hx, hy in house:
         # 가장 가까운 치킨집 찾기
         temp = int(1e9)
         for cx, cy in candidate:
-            temp = min(temp, abs(hx - cx) + abs(hy - cy))   # abs(): 절대값 함수
+            temp = min(temp, abs(hx - cx) + abs(hy - cy))
         # 가장 가까운 치킨집까지의 거리 더하기
-        result += temp
-    # 치킨 거리의 합 반환
-    return result
+        value += temp
+    return value
 
-# 치킨 거리의 합의 최소를 찾아 출력
-result = int(1e9)
-for candidate in candidates:
-    result = min(result, get_sum(candidate))
+ressult = int(1e9)
+for i in candidates:
+    result = min(result, sum(i))
 
 print(result)
