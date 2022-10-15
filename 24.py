2f19fa490501ea16cb017deb85429d3a35b17ed3
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-15 16:09:55 +0900

    Update 24.py

--- 24.py
+++ 24.py
@@ -4,5 +4,5 @@ array = list(map(int, input().split()))
 
 array.sort()
 # index가 n // 2가 아니라 (n - 1) // 2 (이해 안되면 숫자 넣어보기)
-result = array[(n - 1) // 2]
+result = array[(n - 1) // 2]    # array의 중간 값
 print(result)
