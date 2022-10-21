857ac18381eb6e388f29632e4239546d39f9420e
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-21 13:16:32 +0900

    Update 41.py

--- 41.py
+++ 41.py
@@ -50,3 +50,51 @@ else:
 따라서 두 노드 사이에 도로가 존재하는 경우 union 연산을 이용해 서로 연결된 두 노드를 같은 집합에 속하도록 만든다.
 결과적으로 '여행 계획'에 포함되는 모든 노드가 모두 같은 집합에 속하는지 체크(즉, 서로소 집합)
 """    
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+graph = []
+road = []   # 간선 정보
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    for j in range(n):
+        if graph[i][j] == 1:
+            road.append((i + 1, j + 1))
+
+plan = list(map(int, input().split()))
+
+# union연산 각각 수행
+for i in range(len(road)):
+    a, b = road[i]
+    union(parent, a, b)
+
+# 각 원소의 루트 노드 탐색
+for i in range(1, n + 1):
+    find(parent, i)
+
+result = True
+for i in range(m - 1):
+    # 여행 계획의 속한 모든 노드의 루트가 동일한지 확인
+    if find(parent, plan[i]) != find(parent, plan[i + 1]):
+        result = False
+
+if result:
+    print('YES')
+else:
+    print('NO')
