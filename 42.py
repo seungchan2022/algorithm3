215052724b0b33be2d6d8b3738237676c2369da5
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-21 14:00:26 +0900

    Update 42.py

--- 42.py
+++ 42.py
@@ -1,32 +1,39 @@
-def find_parent(parent, x):
+def find(parent, x):
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+        parent[x] = find(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
     if a < b:
         parent[b] = a
     else:
         parent[a] = b
-    
-g = int(input())
-p = int(input())
-parent = [0] * (g + 1)
-for i in range(1, g + 1):
-    parent[i] = i
 
-result = 0
+g = int(input())    # 탑승구 수
+p = int(input())    # 비행기 수
+
+array = []      # 비행기가 도킹할 수 있는 탑승구 정보
 for _ in range(p):
-    # 현재 비행기의 탑승구의 루트 확인(가능한 큰 번호의 탑승구로 도킹 하기 위해)
-    data = find_parent(parent, int(input()))
-    if data == 0:   # 현재 루트가 0이라면, 종료
-        break
-    union_parent(parent, data, data - 1)    # 그렇지 않으면 바로 왼쪽의 집합과 합치기
-    result += 1
+    array.append(int(input()))
 
-print(result)
+parent = [i for i in range(g + 1)]
+
+count = 0
+for i in array:
+    root = find(parent, i)
+
+    # 탑승구 루트가 0이면 break
+    if root == 0:
+        break
+
+    # 탑승구를 최대 번호에 도킹하고, -1 만큼 작은 탑승구를 root로(바로 왼쪽의 집합과 합치기)
+    union(parent, root, root - 1)
+    count += 1
+
+print(count)
+
 
 """
 각 탑승구를 서로 다른 집합으로 보자
@@ -37,3 +44,5 @@ print(result)
 항상 부모 테이블을 만들 때, 0번 루트 노드가 자동으로 생기는데 해당 문제에서 특정 탑승구 번호를 부여 받았을때,
 find 연산을 수행해서 0번 루트 노드이면 더 이상 도킹할 수 없다고 만듬
 """
+
+# https://velog.io/@thguss/%EC%BD%94%ED%85%8C-%EC%8A%A4%ED%84%B0%EB%94%94-%EA%B7%B8%EB%9E%98%ED%94%84-%EC%9D%B4%EB%A1%A0-%ED%83%91%EC%8A%B9%EA%B5%AC
